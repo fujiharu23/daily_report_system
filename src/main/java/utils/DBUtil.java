4430d8edbd793df0434528243c7b7f3929368c6d
haruka.fujimura <fujimura@primagest.co.jp>
2021-08-03 16:26:24 +0900

    DBUtil作成

---
+++ src/main/java/utils/DBUtil.java
@@ -0,0 +1,25 @@
+package utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import constants.JpaConst;
+
+public class DBUtil {
+
+    private static EntityManagerFactory emf;
+
+    //EntityManagerインスタンスを生成
+    public static EntityManager createEntityManager() {
+        return _getEntityManagerFactory().createEntityManager();
+    }
+
+    //EntityManagerFactoryインスタンスを製紙
+    private static EntityManagerFactory _getEntityManagerFactory() {
+        if(emf ==null) {
+            emf = Persistence.createEntityManagerFactory(JpaConst.PERSISTEMCE_UNIT_NAME);
+        }
+        return emf;
+    }
+}
